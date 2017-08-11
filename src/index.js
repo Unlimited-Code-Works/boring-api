b782e1de85c6e17378bacb37b1310a9436ad7f2e
AkashiWen <everstrike@qq.com>
2017-08-11 11:18:29 +0800

    feat: init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const unusued = 'I have no purpose!';
+
+/**
+ * sss
+ */
+function greet() {
+    const message = 'Hello, World!';
+    console.log(`message: ${message}, un: ${unusued}`);
+}
+
+greet();
